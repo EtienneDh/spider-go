51cd3cc34c3bfe3e5c720c779814f19297a35afd
Etienne <etienne@weglot.com>
2020-02-21 14:16:56 +0100

    refactored

--- bcrawler.go
+++ bcrawler.go
@@ -14,6 +14,7 @@ type Url struct {
 
 func main() {	
 
+	// Get config from cmd args:
 	inputUrl := flag.String("url", "https://weglot.com", "Url to crawl")
 	inputDepth := flag.Int("depth", 1, "Depth to crawl")
 	allowedHost := flag.String("host", "", "Allowed host")
@@ -36,10 +37,8 @@ func main() {
 
 	time.Sleep(2000 * time.Millisecond)
 
-	// Instantiate default collector
-	c := colly.NewCollector(
-		// MaxDepth is 2, so only the links on the scraped page
-		// and links on those pages are visited
+	// setup Collector
+	c := colly.NewCollector(		
 		colly.MaxDepth(*inputDepth),
 		colly.Async(true),		
 		colly.AllowedDomains(*allowedHost),
@@ -47,28 +46,21 @@ func main() {
 	)
 	c.AllowURLRevisit = false
 
-	fmt.Println("---------------------------------")
-	fmt.Println("Starting...")
-	fmt.Println("---------------------------------")
-
 	urls := make([]Url, 0, 200)
 
-	// Limit the maximum parallelism to 2
-	// This is necessary if the goroutines are dynamically
-	// created to control the limit of simultaneous requests.
-	//
-	// Parallelism can be controlled also by spawning fixed
-	// number of go routines.
+	fmt.Println("---------------------------------")
+	fmt.Println("Starting...")
+	fmt.Println("---------------------------------")	
+	
 	c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 2})
 
 	// On every a element which has href attribute call callback
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
-		url := Url{link}	
-
-		// Print link
+		url := Url{link}
+		
 		fmt.Println(link)
-		// Visit link found on page on a new thread
+		
 		e.Request.Visit(link)
 
 		urls = append(urls, url)
