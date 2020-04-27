6e4e198a4b8f7154b68345db3c0e99a734b50b29
Etienne <etienne@weglot.com>
2020-04-27 17:14:34 +0200

    Finished refactoring Config for new specs

--- config.go
+++ config.go
@@ -1,64 +1,85 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"net/url"
+	"strings"
 )
 
 type Config struct {
-	Url        string
-	Domain     string
-	Depth      int
-	WriteToCsv bool
-	Private    bool
-	MaxRequest int
-	Count      bool
+	URLS        []string
+	Domain      string
+	Private     bool
+	Integration string
+	Mode        string
 }
 
+var allowedModes = []string{"discover", "crawl"}
+var allowedIntegrations = []string{"javascript", "connect", "wordpress plugin", "shopify app"}
+
 const defaultURL = "https://weglot.com"
-const defaultDepth = 1
+const defaultDepth = 5
 const defaultDomain = ""
-const defaultCSV = false
 const defaultPrivate = false
 const defaultMaxRequest = -1
 const defaultCount = false
 
 const weglotPrivate = "?weglot-private=1"
 
-func getConfig() Config {
-	// Get config from cmd args:
-	inputUrl := flag.String("url", defaultURL, "Url to crawl")
-	inputDepth := flag.Int("depth", defaultDepth, "Depth to crawl")
-	allowedDomain := flag.String("domain", defaultDomain, "Allowed domain")
-	writeCsv := flag.Bool("csv", defaultCSV, "Writes results to CSV")
-	private := flag.Bool("private", defaultPrivate, "Crawls with private mode")
-	max := flag.Int("max", defaultMaxRequest, "Maximum requests to perform")
-	count := flag.Bool("crawl", defaultCount, "Count words")
+// NewConfig set up and return a new Config type
+func NewConfig() (Config, error) {
 
+	// Get config from cmd args:
+	inputURLS := flag.String("url", defaultURL, "Url to crawl")
+	private := flag.Bool("private", defaultPrivate, "Crawls with private mode")
+	integration := flag.String("integration", "", "Project Integration")
+	mode := flag.String("mode", "discover", "Bot mode: discover or crawl")
 	flag.Parse()
 
-	config := Config{*inputUrl, *allowedDomain, *inputDepth, *writeCsv, *private, *max, *count}
-	config.init()
+	// extract urls
+	urls := getURLS(*inputURLS)
+	if len(urls) == 0 {
+		return Config{}, errors.New("You must enter at least 1 url")
+	}
+	// resolve host
+	domain := getDomain(urls[0])
 
-	return config
+	// validate
+	if !isValid(*integration, allowedIntegrations) {
+		return Config{}, errors.New("You must enter a valid integration; javascript, connect, wordpress plugin or shopify app")
+	}
+	if !isValid(*mode, allowedModes) {
+		return Config{}, errors.New("You must enter a valid mode: discover or crawl")
+	}
+	config := Config{urls, domain, *private, *integration, *mode}
+
+	return config, nil
+}
+
+func getURLS(urlsAsString string) []string {
+
+	return strings.Split(urlsAsString, " ")
 }
 
 // Config is passed as pointer to allow modifying the struct
-func (config *Config) init() {
-	// resolve domain if not passed as arg
-	if len(config.Domain) == 0 {
-		u, err := url.Parse(config.Url)
-		if err != nil {
-			fmt.Println("Failed to resolve host")
-			panic(err)
-		}
-		host := u.Host
-		config.Domain = host
+func getDomain(inputURL string) string {
+	u, err := url.Parse(inputURL)
+	if err != nil {
+		fmt.Println("Failed to resolve host")
+		panic(err)
 	}
 
-	// update url for private mode
-	if config.Private {
-		config.Url = config.Url + weglotPrivate
+	return u.Host
+}
+
+func isValid(toValidate string, authorizedValues []string) bool {
+	for _, value := range authorizedValues {
+		if toValidate == value {
+			return true
+		}
 	}
+
+	return false
 }
--- crawler.go
+++ crawler.go
@@ -1,6 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
-	weglotCrawler := NewWeglotCrawler()
-	weglotCrawler.Run()
+	// Maybe some type of configs will have an effect here
+	// Like if we're in generate content mode and receive a list of URLs
+	// It may be easier to run len(URLS) times the bot
+
+	// To achieve this, I would need to refacto the config out of weglotCrawler.initialize
+	// and instead pass it as args to New method
+
+	config, err := NewConfig()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(config)
+	// weglotCrawler := NewWeglotCrawler()
+	// weglotCrawler.Run()
 }
